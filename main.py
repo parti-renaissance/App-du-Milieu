bb6e229f3c71991884ff2825879d05452c1cf878
arniverd <75568654+arniverd@users.noreply.github.com>
2021-04-08 09:07:22 +0200

    Adding CORS

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from os import environ
 from typing import List
 
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
 from app import crud, models, schemas
@@ -17,6 +18,19 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "http://localhost:3002"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Dependency
 def get_db():
