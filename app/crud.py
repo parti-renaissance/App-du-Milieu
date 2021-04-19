88a389ec0ff8652871d5b6dfb775425553409214
arniverd <75568654+arniverd@users.noreply.github.com>
2021-04-19 17:12:38 +0200

    Adding crud to get region from candidate_managed_area_id

--- app/crud.py
+++ app/crud.py
@@ -9,9 +9,8 @@ from app.dependencies import CommonQueryParams
 
 
 def get_contacts(db: Session, commons: CommonQueryParams):
-    meta = {}
-    meta.update({'interests_choices': schemas.Interests_choices.list()})
-    meta.update({'gender_choices': schemas.Gender.list()})
+    interests = {'interests_choices': schemas.Interests_choices.list()}
+    gender = {'gender_choices': schemas.Gender.list()}
 
     filter_query = {}
     if commons.code_postal:
@@ -23,11 +22,51 @@ def get_contacts(db: Session, commons: CommonQueryParams):
 
     contacts = {'contacts' : [contact.serialize() for contact in
             db.query(models.Contact).filter_by(**filter_query).all()]}
-    meta.update({'total_items': len(contacts['contacts'])})
-    meta.update(contacts)
 
-    return meta
+    return {
+        'total_items': len(contacts['contacts']),
+        **interests,
+        **gender,
+        **contacts
+        }
 
 
 def get_contact(db: Session, id: int):
-    return db.query(models.Contact).filter(models.Contact.id == id).first()
+    return db.query(models.Contact) \
+             .filter(models.Contact.id == id) \
+             .first()
+
+
+def me(db: Session, uuid: str):
+    adherent = db.query(models.Adherents) \
+                 .filter(models.Adherents.uuid == uuid) \
+                 .first()
+    if adherent is None:
+        return None
+    return adherent
+
+
+def get_candidate_region(db: Session, uuid: str):
+    adherent = me(db, uuid)
+    if adherent is None:
+        return None
+
+    managedArea = db.query(models.CandidateManagedArea) \
+                    .filter(models.CandidateManagedArea.id == adherent.get_candidate_managed_area()) \
+                    .first()
+    if managedArea is None:
+        return None
+
+    geoZone = db.query(models.GeoZone) \
+                .filter(models.GeoZone.id == managedArea.get_zone_id()) \
+                .first()
+    if geoZone is None:
+        return None
+
+    geoRegion = db.query(models.GeoRegion) \
+                .filter(models.GeoRegion.code == geoZone.get_code()) \
+                .first()
+    if geoRegion is None:
+        return None
+
+    return geoRegion
--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 """
 SQLAlchemy de notre base de données
 """
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, UniqueConstraint
 
 from app.database import Base
 
@@ -79,3 +79,61 @@ class Contact(Base):
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
+
+
+class Adherents(Base):
+    """ Table adherents id/uuid """
+    __tablename__ = "adherents"
+
+    id = Column(Integer, primary_key=True, index=True)
+    uuid = Column(String(36), unique=True, nullable=False, index=True)
+    candidate_managed_area_id = Column(Integer)
+
+
+    def get_candidate_managed_area(self):
+        return self.candidate_managed_area_id
+
+
+class CandidateManagedArea(Base):
+    """ Table candidate_managed_area pour retrouver la zone_id """
+    __tablename__ = "candidate_managed_area"
+
+    id = Column(Integer, primary_key=True, index=True)
+    zone_id = Column(Integer, nullable=False)
+
+
+    def get_zone_id(self):
+        return self.zone_id
+
+
+class GeoZone(Base):
+    """ Table candidate_managed_area pour retrouver la zone_id """
+    __tablename__ = "geo_zone"
+
+    id = Column(Integer, primary_key=True, index=True)
+    type = Column(String(255), nullable=False)
+    code = Column(String(255), nullable=False)
+    name = Column(String(255), nullable=False)
+    
+    UniqueConstraint('code', 'type', name='geo_zone_code_type_unique')
+
+
+    def get_code(self):
+        return self.code
+
+
+class GeoRegion(Base):
+    """ Table candidate_managed_area pour retrouver la zone_id """
+    __tablename__ = "geo_region"
+
+    id = Column(Integer, primary_key=True, index=True)
+    code = Column(String(255), unique=True, nullable=False, index=True)
+    name = Column(String(255), nullable=False)
+
+
+    def get_code(self):
+        return self.code
+
+
+    def get_name(self):
+        return self.name
