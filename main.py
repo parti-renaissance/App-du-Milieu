65758d8a7c9241f3181c19b075a6f0b9310faa8b
arniverd <75568654+arniverd@users.noreply.github.com>
2021-06-04 10:17:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -185,7 +185,7 @@ def get_available_elections():
 
 if __name__ == "__main__":
     uvicorn.run(
-        app=,
+        app,
         host="0.0.0.0",
         port=int(environ.get("PORT", 8080))
     	)
