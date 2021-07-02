94c8eaa1cdba6e92631462d0fb4d597783e22032
arniverd <75568654+arniverd@users.noreply.github.com>
2021-07-02 17:04:19 +0200

    Revert "Scopes"

--- app/crud/enmarche.py
+++ app/crud/enmarche.py
@@ -1,88 +1,34 @@
 """
 Endpoints de notre api
 """
-from collections import defaultdict
-from enum import Enum
-import base64
-import json
-
 from sqlalchemy.orm import Session
 from sqlalchemy import and_
 
-from app.models.models_enmarche import GeoZone, GeoZoneParent
+from app.models.models_enmarche import Adherents, CandidateManagedArea, GeoZone, GeoZoneParent
 
 
-class GeoTypes(str, Enum):
-    '''
-        On ajoute ici les colonnes implementees dans la table contact pour pouvoir filtrer dessus
-    '''
-    #borough = 'arrondissement'
-    #canton = 'canton'
-    city = 'commune'
-    #city_community = 'communaute_de_communes'
-    #consular_district = 'circonscription_consulaire'
-    #country = 'pays'
-    department = 'departement'
-    #district = 'circonscription'
-    #foreign_district = 'circonscription_des_FDE'
-    region = 'region'
+def me(db: Session, uuid: str) -> Adherents:
+    if adherent := db.query(Adherents) \
+                    .filter(Adherents.uuid == uuid) \
+                    .first():
+        return adherent
 
 
-def getGeoType(s: str):
-    '''
-        Retourne le nom equivalent a la colonne de la table contact d'un type de GeoZone
-    '''
-    for t in GeoTypes:
-        if t.name == s:
-            return t.value
-    return False
+def get_candidate_zone(db: Session, uuid: str):
+    return db.query(GeoZone) \
+             .join(CandidateManagedArea, CandidateManagedArea.zone_id == GeoZone.id) \
+             .join(Adherents, Adherents.candidate_managed_area_id == CandidateManagedArea.id) \
+             .filter(Adherents.uuid == uuid) \
+             .first()
 
 
-def scope2dict(scope: dict, name: bool = False):
-    '''
-        Transforme {'code':'roles', 'zones':[Liste de GeoZone]}
-        en dict utilisable par sqlalchemy pour filtrer les zones
-    '''
-    res = defaultdict(list)
-    for sub in scope['zones']:
-        if (type := getGeoType(sub.type)):
-            if name:
-                res[type].append(sub.name)
-            else:
-                res['code_'+type].append(sub.code)
-
-    return res
-
-
-def decode_scopes(db: Session, scope: str):
-    '''
-        Recupere le X-Scope du header et le decode en base64
-    '''
-    base64_bytes = scope.encode("latin1")
-    scope_bytes = base64.b64decode(base64_bytes)
-    scope_string = scope_bytes.decode("latin1")  
-    scope_dict = json.loads(scope_string)
-
-    res_zone = []
-    for zone in scope_dict['zones']:
-        res_zone = [*res_zone,
-            db.query(GeoZone) \
-              .filter(GeoZone.uuid == zone['uuid']) \
-              .first()]
-    return {'code':scope_dict['code'], 'zones':res_zone}
-
-
-def get_child(db: Session, parent: GeoZone, type: str = None):
+def get_child_code(db: Session, parent: GeoZone, type: str):
     if parent.type == type:
         return parent
 
-    query = db.query(GeoZone) \
+    return db.query(GeoZone.code) \
         .join(GeoZoneParent, and_(
             GeoZoneParent.child_id  == GeoZone.id,
-            GeoZoneParent.parent_id == parent.id))
-    
-    if type:
-        query = query.filter(GeoZone.type == type)
-    
-    return query.all()
-
+            GeoZoneParent.parent_id == parent.id)) \
+        .filter(GeoZone.type == type) \
+        .all()
--- app/crud/mail_campaign.py
+++ app/crud/mail_campaign.py
@@ -2,47 +2,23 @@
 Endpoints de notre api
 """
 from datetime import datetime
-from typing import List
-from sqlalchemy.orm import Session, Query
+from sqlalchemy.orm import Session
+from sqlalchemy.sql import select
 from sqlalchemy import func
-from app.crud.enmarche import get_child
 from app.models.models_enmarche import AdherentMessages, Adherents, CandidateManagedArea
-from app.models.models_enmarche import AdherentMessageFilters, ReferentTags, ReferentManagedAreasTags
 from app.models.models_enmarche import MailChimpCampaign, MailChimpCampaignReport
 from app.models.models_enmarche import GeoZone
 
 
-def filter_role(
-    db: Session,
-    query: Query,
-    zones: List[GeoZone],
-    role: str):
-    all_zones = [[zone.id] + [child.id for child in get_child(db, zone)] for zone in zones]
-    all_zones = [item for sublist in all_zones for item in sublist]
-    
-    if role == 'referent':
-        return query.join(Adherents.managed_area) \
-            .join(ReferentManagedAreasTags.referent_tag) \
-            .join(ReferentTags.zone.and_(GeoZone.id.in_(all_zones)))
-
-    if role in ['deputy', 'senator']:
-        return query.join(AdherentMessages.filter) \
-            .join(AdherentMessageFilters.referent_tag) \
-            .join(ReferentTags.zone.and_(GeoZone.id.in_(all_zones)))
-            
-    if role == 'candidate':
-        return query.join(AdherentMessages.filter) \
-            .join(AdherentMessageFilters.zone.and_(GeoZone.id.in_(all_zones)))
-    return query
-
-
-async def get_campaign_reports(
+async def get_candidate_reports(
     db: Session,
     zone: GeoZone,
-    since: datetime,
-    role: str):
+    since: datetime):
 
-    query = db.query(
+    return {
+        'zone': zone.name,
+        'depuis': since,
+        'campagnes': db.query(
             MailChimpCampaign.id.label('id'), \
             AdherentMessages.sent_at.label('date'), \
             (Adherents.first_name + ' ' + Adherents.last_name).label('auteur'), \
@@ -56,37 +32,33 @@ async def get_campaign_reports(
             func.round(MailChimpCampaignReport.unsubscribed / MailChimpCampaignReport.email_sent, 4).label('txDesabonnement')) \
         .join(MailChimpCampaign.message) \
         .filter(AdherentMessages.status == 'sent') \
-        .filter(AdherentMessages.type == role) \
+        .filter(AdherentMessages.type.in_(['candidate', 'candidate_jecoute'])) \
         .filter(AdherentMessages.sent_at >= since) \
         .join(MailChimpCampaign.report) \
-        .join(AdherentMessages.author)
-    
-    query = filter_role(db, query, [zone], role)
-
-    return {
-        'zone': zone.name,
-        'depuis': since,
-        'campagnes': query.order_by(AdherentMessages.sent_at.desc()).all()
+        .join(AdherentMessages.author) \
+        .join(Adherents.candidate_managed_area.and_(CandidateManagedArea.zone_id == zone.id)) \
+        .order_by(AdherentMessages.sent_at.desc()) \
+        .all()
     }
 
 
 async def get_mail_ratios(
     db: Session,
-    scope: dict,
+    zone: GeoZone,
     since: datetime):
 
-    query = db.query(func.count(MailChimpCampaign.id).label('nbCampagnes'), \
+    res = db.query(func.count(MailChimpCampaign.id).label('nbCampagnes'), \
             func.round(func.sum(MailChimpCampaignReport.open_unique) / func.sum(MailChimpCampaignReport.email_sent), 4).label('txOuverture'), \
             func.round(func.sum(MailChimpCampaignReport.click_unique) / func.sum(MailChimpCampaignReport.email_sent), 4).label('txClique'), \
             func.round(func.sum(MailChimpCampaignReport.unsubscribed) / func.sum(MailChimpCampaignReport.email_sent), 4).label('txDesabonnement')) \
         .select_from(MailChimpCampaignReport) \
         .join(MailChimpCampaignReport.mailchimp_campaign) \
         .join(MailChimpCampaign.message.and_( \
-            AdherentMessages.type == scope['code'], \
+            AdherentMessages.type.in_(['candidate', 'candidate_jecoute']), \
             AdherentMessages.sent_at >= since)) \
-        .join(AdherentMessages.author)
-    
-    res = filter_role(db, query, scope['zones'], scope['code']).first()
+        .join(AdherentMessages.author) \
+        .join(Adherents.candidate_managed_area.and_(CandidateManagedArea.zone_id == zone.id)) \
+        .first()
 
     nat = db.query(
             func.round(func.sum(MailChimpCampaignReport.open_unique) / func.sum(MailChimpCampaignReport.email_sent), 4).label('txOuverture'), \
@@ -95,7 +67,7 @@ async def get_mail_ratios(
         .select_from(MailChimpCampaignReport) \
         .join(MailChimpCampaignReport.mailchimp_campaign) \
         .join(MailChimpCampaign.message.and_( \
-            AdherentMessages.type == scope['code'])) \
+            AdherentMessages.type.in_(['candidate', 'candidate_jecoute']))) \
         .first()
 
     return {'local': res, 'national': nat}
