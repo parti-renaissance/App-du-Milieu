2c62244335abe3091a3f2ce5324e83900421497d
Mickael Labarrere <35226465+mlabarrere@users.noreply.github.com>
2021-07-27 13:20:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -29,10 +29,10 @@ app.add_middleware(GZipMiddleware, minimum_size=1000)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=['*'],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=['*'],
+    allow_headers=['*'],
 )
 
 # app.add_middleware(PyInstrumentProfilerMiddleware)
