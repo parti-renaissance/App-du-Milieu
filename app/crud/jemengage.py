c8ddaf3bc8e3c09d75e37a3c58d8fbfad950a69c
Mickael Labarrere <35226465+mlabarrere@users.noreply.github.com>
2021-08-02 17:52:52 +0200

    Crud propre

--- app/crud/jemengage.py
+++ app/crud/jemengage.py
@@ -5,165 +5,174 @@ import pandas as pd
 from app.crud.enmarche import get_child, scope2dict
 from app.database.database_crm import engine_crm
 from app.models.models_crm import Downloads, Users
-from app.models.models_enmarche import (GeoBorough, GeoCity, GeoCountry,
-                                        GeoDepartment, GeoDistrict, GeoRegion,
-                                        JecouteDataSurvey)
+from app.models.models_enmarche import (
+    GeoBorough,
+    GeoCity,
+    GeoCountry,
+    GeoDepartment,
+    GeoDistrict,
+    GeoRegion,
+    JecouteDataSurvey,
+)
 from sqlalchemy import Date
 from sqlalchemy.orm import Session, joinedload
 
 
 def get_downloads(
-    db: Session,
-    scope: dict,
-    before: Date = date.today(),
-    range_days: int = 28
+    db: Session, scope: dict, before: Date = date.today(), range_days: int = 28
 ):
     after = before - timedelta(days=range_days)
 
     # series of all date from min to max
     s_date = pd.date_range(after, before - timedelta(days=1))
 
-    empty = pd.DataFrame(s_date, columns=['date'])
-    empty['unique_user'] = 0
+    empty = pd.DataFrame(s_date, columns=["date"])
+    empty["unique_user"] = 0
     df_list = [empty]
 
     for k, v in scope2dict(scope, True).items():
-        query = db.query(Downloads.date, Downloads.unique_user) \
-            .filter(Downloads.zone_type == k) \
-            .filter(Downloads.zone_name.in_(v)) \
-            .filter(Downloads.date < before) \
-            .filter(Downloads.date >= after) \
+        query = (
+            db.query(Downloads.date, Downloads.unique_user)
+            .filter(Downloads.zone_type == k)
+            .filter(Downloads.zone_name.in_(v))
+            .filter(Downloads.date < before)
+            .filter(Downloads.date >= after)
             .statement
+        )
 
         df = pd.read_sql(query, engine_crm)
         if not df.empty:
             # fill missing date
-            df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
-            df = df.groupby('date').sum().reindex(s_date).rename_axis('date')
+            df["date"] = pd.to_datetime(df["date"], format="%Y-%m-%d")
+            df = df.groupby("date").sum().reindex(s_date).rename_axis("date")
             # fill unique user to 0
-            df['unique_user'] = df['unique_user'].fillna(0).astype(int)
+            df["unique_user"] = df["unique_user"].fillna(0).astype(int)
 
             df.reset_index(inplace=True)
             df_list = [*df_list, df]
 
     big_df = pd.concat(df_list)
-    big_df = big_df.groupby(['date']).sum().reset_index()
-    big_df['date'] = big_df['date'].dt.strftime('%d/%m')
+    big_df = big_df.groupby(["date"]).sum().reset_index()
+    big_df["date"] = big_df["date"].dt.strftime("%d/%m")
 
     return big_df
 
 
 def get_users(
-    db: Session,
-    scope: dict,
-    before: Date = date.today(),
-    range_days: int = 28
+    db: Session, scope: dict, before: Date = date.today(), range_days: int = 28
 ):
     after = before - timedelta(days=range_days)
 
     # series of all date from min to max
     s_date = pd.date_range(after - timedelta(7), before - timedelta(days=1))
 
-    empty = pd.DataFrame(s_date, columns=['date'])
-    empty['unique_user'] = 0
+    empty = pd.DataFrame(s_date, columns=["date"])
+    empty["unique_user"] = 0
     df_list = [empty]
 
     for k, v in scope2dict(scope, True).items():
-        query = db.query(Users.date, Users.unique_user) \
-            .filter(Users.zone_type == k) \
-            .filter(Users.zone_name.in_(v)) \
-            .filter(Users.date < before) \
-            .filter(Users.date >= after - timedelta(7)) \
+        query = (
+            db.query(Users.date, Users.unique_user)
+            .filter(Users.zone_type == k)
+            .filter(Users.zone_name.in_(v))
+            .filter(Users.date < before)
+            .filter(Users.date >= after - timedelta(7))
             .statement
+        )
 
         df = pd.read_sql(query, engine_crm)
         if not df.empty:
             # fill missing date
-            df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
-            df = df.groupby('date').sum().reindex(s_date).rename_axis('date')
+            df["date"] = pd.to_datetime(df["date"], format="%Y-%m-%d")
+            df = df.groupby("date").sum().reindex(s_date).rename_axis("date")
             # fill unique user to 0
-            df['unique_user'] = df['unique_user'].fillna(0).astype(int)
+            df["unique_user"] = df["unique_user"].fillna(0).astype(int)
             # fill cumulative to previous value
-            df['rolling_seven_users'] = df.rolling(
-                7).sum().fillna(0).astype(int)
+            df["rolling_seven_users"] = df.rolling(7).sum().fillna(0).astype(int)
 
             df.reset_index(inplace=True)
             df = df[df.date >= pd.to_datetime(after)]
             df_list = [*df_list, df]
 
     big_df = pd.concat(df_list)
-    big_df = big_df.groupby(['date']).sum().reset_index()
-    big_df['date'] = big_df['date'].dt.strftime('%d/%m')
+    big_df = big_df.groupby(["date"]).sum().reset_index()
+    big_df["date"] = big_df["date"].dt.strftime("%d/%m")
 
     return big_df
 
 
-def get_survey(
-    db: Session,
-    scope: dict
-):
+def get_survey(db: Session, scope: dict):
 
     city_codes = []
-    for zone in scope['zones']:
-        city_codes += [zone.code for zone in get_child(db, zone, 'city')]
+    for zone in scope["zones"]:
+        city_codes += [zone.code for zone in get_child(db, zone, "city")]
 
-    survey_datas = db.query(JecouteDataSurvey) .options(
-        joinedload(
-            JecouteDataSurvey.author)) .options(
-        joinedload(
-            JecouteDataSurvey.survey)) .filter(
-        JecouteDataSurvey.postal_code != '') .join(
-        GeoCity,
-        GeoCity.postal_code.like(
-            '%' +
-            JecouteDataSurvey.postal_code +
-            '%')) .filter(
-        GeoCity.code.in_(city_codes)) .filter(
-        JecouteDataSurvey.latitude != '') .filter(
-        JecouteDataSurvey.longitude != '') .all()
+    survey_datas = (
+        db.query(JecouteDataSurvey)
+        .options(joinedload(JecouteDataSurvey.author))
+        .options(joinedload(JecouteDataSurvey.survey))
+        .filter(JecouteDataSurvey.postal_code != "")
+        .join(
+            GeoCity, GeoCity.postal_code.like("%" + JecouteDataSurvey.postal_code + "%")
+        )
+        .filter(GeoCity.code.in_(city_codes))
+        .filter(JecouteDataSurvey.latitude != "")
+        .filter(JecouteDataSurvey.longitude != "")
+        .all()
+    )
 
     returned_zone = scope2dict(scope, True)
     res = {}
-    if 'pays' in returned_zone.keys():
-        geo_nat = db.query(GeoCountry) \
-            .filter(GeoCountry.name == returned_zone['pays'][0]) \
+    if "pays" in returned_zone.keys():
+        geo_nat = (
+            db.query(GeoCountry)
+            .filter(GeoCountry.name == returned_zone["pays"][0])
             .first()
-        res['zone_name'] = geo_nat.name
-        res['latitude'] = geo_nat.latitude
-        res['longitude'] = geo_nat.longitude
-    elif 'region' in returned_zone.keys():
-        geo_reg = db.query(GeoRegion) \
-            .filter(GeoRegion.name == returned_zone['region'][0]) \
+        )
+        res["zone_name"] = geo_nat.name
+        res["latitude"] = geo_nat.latitude
+        res["longitude"] = geo_nat.longitude
+    elif "region" in returned_zone.keys():
+        geo_reg = (
+            db.query(GeoRegion)
+            .filter(GeoRegion.name == returned_zone["region"][0])
             .first()
-        res['zone_name'] = geo_reg.name
-        res['latitude'] = geo_reg.latitude
-        res['longitude'] = geo_reg.longitude
-    elif 'departement' in returned_zone.keys():
-        geo_dpt = db.query(GeoDepartment) \
-            .filter(GeoDepartment.name == returned_zone['departement'][0]) \
+        )
+        res["zone_name"] = geo_reg.name
+        res["latitude"] = geo_reg.latitude
+        res["longitude"] = geo_reg.longitude
+    elif "departement" in returned_zone.keys():
+        geo_dpt = (
+            db.query(GeoDepartment)
+            .filter(GeoDepartment.name == returned_zone["departement"][0])
             .first()
-        res['zone_name'] = geo_dpt.name
-        res['latitude'] = geo_dpt.latitude
-        res['longitude'] = geo_dpt.longitude
-    elif 'circonscription' in returned_zone.keys():
-        geo_district = db.query(GeoDistrict) \
-            .filter(GeoDistrict.name == returned_zone['circonscription'][0]) \
+        )
+        res["zone_name"] = geo_dpt.name
+        res["latitude"] = geo_dpt.latitude
+        res["longitude"] = geo_dpt.longitude
+    elif "circonscription" in returned_zone.keys():
+        geo_district = (
+            db.query(GeoDistrict)
+            .filter(GeoDistrict.name == returned_zone["circonscription"][0])
             .first()
-        res['zone_name'] = geo_district.name
-        res['latitude'] = geo_district.latitude
-        res['longitude'] = geo_district.longitude
-    elif 'arrondissement_commune' in returned_zone.keys():
-        geo_borough = db.query(GeoBorough) \
-            .filter(GeoBorough.name == returned_zone['arrondissement_commune'][0]) \
+        )
+        res["zone_name"] = geo_district.name
+        res["latitude"] = geo_district.latitude
+        res["longitude"] = geo_district.longitude
+    elif "arrondissement_commune" in returned_zone.keys():
+        geo_borough = (
+            db.query(GeoBorough)
+            .filter(GeoBorough.name == returned_zone["arrondissement_commune"][0])
             .first()
-        res['zone_name'] = geo_borough.name
-        res['latitude'] = geo_borough.latitude
-        res['longitude'] = geo_borough.longitude
+        )
+        res["zone_name"] = geo_borough.name
+        res["latitude"] = geo_borough.latitude
+        res["longitude"] = geo_borough.longitude
     else:
-        res['zone_name'] = next(iter(returned_zone.values()))[0]
+        res["zone_name"] = next(iter(returned_zone.values()))[0]
         # Chez Xavier
-        res['latitude'] = 48.835633
-        res['longitude'] = 2.323433
+        res["latitude"] = 48.835633
+        res["longitude"] = 2.323433
 
-    res['survey_datas'] = survey_datas
+    res["survey_datas"] = survey_datas
     return res
--- app/crud/mail_campaign.py
+++ app/crud/mail_campaign.py
@@ -3,153 +3,164 @@ from datetime import datetime
 from typing import List
 
 from app.crud.enmarche import get_child
-from app.models.models_enmarche import (AdherentMessageFilters,
-                                        AdherentMessages, Adherents, GeoZone,
-                                        MailChimpCampaign,
-                                        MailChimpCampaignReport,
-                                        ReferentManagedAreasTags, ReferentTags)
+from app.models.models_enmarche import (
+    AdherentMessageFilters,
+    AdherentMessages,
+    Adherents,
+    GeoZone,
+    MailChimpCampaign,
+    MailChimpCampaignReport,
+    ReferentManagedAreasTags,
+    ReferentTags,
+)
 from sqlalchemy import func
 from sqlalchemy.orm import Query, Session
 from sqlalchemy.sql.functions import coalesce
 
 
-def filter_role(
-        db: Session,
-        query: Query,
-        zones: List[GeoZone],
-        role: str):
+def filter_role(db: Session, query: Query, zones: List[GeoZone], role: str):
     """This function adds geozone filter to query
-    
+
     Allow to filter database
     according to implemented roles
     """
-    all_zones = [[zone.id] +
-                 [child.id for child in get_child(db, zone)] for zone in zones]
+    all_zones = [
+        [zone.id] + [child.id for child in get_child(db, zone)] for zone in zones
+    ]
     all_zones = [item for sublist in all_zones for item in sublist]
 
-    if role == 'referent':
-        return query.join(Adherents.managed_area) \
-            .join(ReferentManagedAreasTags.referent_tag) \
+    if role == "referent":
+        return (
+            query.join(Adherents.managed_area)
+            .join(ReferentManagedAreasTags.referent_tag)
             .join(ReferentTags.zone.and_(GeoZone.id.in_(all_zones)))
+        )
 
-    if role in {'deputy', 'senator'}:
-        return query.join(AdherentMessages.filter) \
-            .join(AdherentMessageFilters.referent_tag) \
+    if role in {"deputy", "senator"}:
+        return (
+            query.join(AdherentMessages.filter)
+            .join(AdherentMessageFilters.referent_tag)
             .join(ReferentTags.zone.and_(GeoZone.id.in_(all_zones)))
+        )
 
-    if role in {'candidate', 'national'}:
-        return query.join(AdherentMessages.filter) \
-            .join(AdherentMessageFilters.zone.and_(GeoZone.id.in_(all_zones)))
+    if role in {"candidate", "national"}:
+        return query.join(AdherentMessages.filter).join(
+            AdherentMessageFilters.zone.and_(GeoZone.id.in_(all_zones))
+        )
     return query
 
 
-async def get_campaign_reports(
-        db: Session,
-        zone: GeoZone,
-        since: datetime,
-        role: str):
+async def get_campaign_reports(db: Session, zone: GeoZone, since: datetime, role: str):
     """Method to CRUD /campaign/reports"""
-    query = db.query(
-        MailChimpCampaign.id.label('id'),
-        AdherentMessages.sent_at.label('date'),
-        (Adherents.first_name +
-         ' ' +
-         Adherents.last_name).label('auteur'),
-        AdherentMessages.subject.label('titre'),
-        MailChimpCampaignReport.email_sent.label('nbEmails'),
-        MailChimpCampaignReport.open_unique.label('nbOuvertures'),
-        func.round(
-            MailChimpCampaignReport.open_unique /
-            MailChimpCampaignReport.email_sent,
-            4).label('txOuverture'),
-        MailChimpCampaignReport.click_unique.label('nbCliques'),
-        func.round(
-            MailChimpCampaignReport.click_unique /
-            MailChimpCampaignReport.email_sent,
-            4).label('txClique'),
-        MailChimpCampaignReport.unsubscribed.label('nbDesabonnements'),
-        func.round(
-                MailChimpCampaignReport.unsubscribed /
-                MailChimpCampaignReport.email_sent,
-                4).label('txDesabonnement')) .join(
-                    MailChimpCampaign.message) .filter(
-                        AdherentMessages.status == 'sent') .filter(
-                            AdherentMessages.sent_at >= since) .join(
-                                MailChimpCampaign.report) .join(
-                                    AdherentMessages.author)
-    if role != 'national':
+    query = (
+        db.query(
+            MailChimpCampaign.id.label("id"),
+            AdherentMessages.sent_at.label("date"),
+            (Adherents.first_name + " " + Adherents.last_name).label("auteur"),
+            AdherentMessages.subject.label("titre"),
+            MailChimpCampaignReport.email_sent.label("nbEmails"),
+            MailChimpCampaignReport.open_unique.label("nbOuvertures"),
+            func.round(
+                MailChimpCampaignReport.open_unique
+                / MailChimpCampaignReport.email_sent,
+                4,
+            ).label("txOuverture"),
+            MailChimpCampaignReport.click_unique.label("nbCliques"),
+            func.round(
+                MailChimpCampaignReport.click_unique
+                / MailChimpCampaignReport.email_sent,
+                4,
+            ).label("txClique"),
+            MailChimpCampaignReport.unsubscribed.label("nbDesabonnements"),
+            func.round(
+                MailChimpCampaignReport.unsubscribed
+                / MailChimpCampaignReport.email_sent,
+                4,
+            ).label("txDesabonnement"),
+        )
+        .join(MailChimpCampaign.message)
+        .filter(AdherentMessages.status == "sent")
+        .filter(AdherentMessages.sent_at >= since)
+        .join(MailChimpCampaign.report)
+        .join(AdherentMessages.author)
+    )
+    if role != "national":
         query = query.filter(AdherentMessages.type == role)
 
     query = filter_role(db, query, [zone], role)
 
     return {
-        'zone': zone.name,
-        'depuis': since,
-        'campagnes': query.order_by(AdherentMessages.sent_at.desc()).all()
+        "zone": zone.name,
+        "depuis": since,
+        "campagnes": query.order_by(AdherentMessages.sent_at.desc()).all(),
     }
 
 
-async def get_mail_ratios(
-        db: Session,
-        scope: dict,
-        since: datetime):
+async def get_mail_ratios(db: Session, scope: dict, since: datetime):
     """Method to CRUD /campaign/reportsRatios"""
-    query = db.query(
-        func.count(
-            MailChimpCampaign.id).label('nbCampagnes'),
-        coalesce(
-            func.round(
-                func.sum(
-                    MailChimpCampaignReport.open_unique) /
-                func.sum(
-                    MailChimpCampaignReport.email_sent),
-                4),
-            0).label('txOuverture'),
-        coalesce(
-            func.round(
-                func.sum(
-                    MailChimpCampaignReport.click_unique) /
-                func.sum(
-                    MailChimpCampaignReport.email_sent),
-                4),
-            0).label('txClique'),
-        coalesce(
-            func.round(
-                func.sum(
-                    MailChimpCampaignReport.unsubscribed) /
-                func.sum(
-                    MailChimpCampaignReport.email_sent),
-                4),
-            0).label('txDesabonnement')) .select_from(MailChimpCampaignReport) .join(
-        MailChimpCampaignReport.mailchimp_campaign) .join(
-        MailChimpCampaign.message.and_(
-            AdherentMessages.type == scope['code'],
-            AdherentMessages.sent_at >= since)) .join(
-        AdherentMessages.author)
+    query = (
+        db.query(
+            func.count(MailChimpCampaign.id).label("nbCampagnes"),
+            coalesce(
+                func.round(
+                    func.sum(MailChimpCampaignReport.open_unique)
+                    / func.sum(MailChimpCampaignReport.email_sent),
+                    4,
+                ),
+                0,
+            ).label("txOuverture"),
+            coalesce(
+                func.round(
+                    func.sum(MailChimpCampaignReport.click_unique)
+                    / func.sum(MailChimpCampaignReport.email_sent),
+                    4,
+                ),
+                0,
+            ).label("txClique"),
+            coalesce(
+                func.round(
+                    func.sum(MailChimpCampaignReport.unsubscribed)
+                    / func.sum(MailChimpCampaignReport.email_sent),
+                    4,
+                ),
+                0,
+            ).label("txDesabonnement"),
+        )
+        .select_from(MailChimpCampaignReport)
+        .join(MailChimpCampaignReport.mailchimp_campaign)
+        .join(
+            MailChimpCampaign.message.and_(
+                AdherentMessages.type == scope["code"],
+                AdherentMessages.sent_at >= since,
+            )
+        )
+        .join(AdherentMessages.author)
+    )
 
-    res = filter_role(db, query, scope['zones'], scope['code']).first()
+    res = filter_role(db, query, scope["zones"], scope["code"]).first()
 
-    nat = db.query(
-        func.round(
-            func.sum(
-                MailChimpCampaignReport.open_unique) /
-            func.sum(
-                MailChimpCampaignReport.email_sent),
-            4).label('txOuverture'),
-        func.round(
-            func.sum(
-                MailChimpCampaignReport.click_unique) /
-            func.sum(
-                MailChimpCampaignReport.email_sent),
-            4).label('txClique'),
-        func.round(
-            func.sum(
-                MailChimpCampaignReport.unsubscribed) /
-            func.sum(
-                MailChimpCampaignReport.email_sent),
-            4).label('txDesabonnement')) .select_from(MailChimpCampaignReport) .join(
-        MailChimpCampaignReport.mailchimp_campaign) .join(
-        MailChimpCampaign.message.and_(
-            AdherentMessages.type == scope['code'])) .first()
+    nat = (
+        db.query(
+            func.round(
+                func.sum(MailChimpCampaignReport.open_unique)
+                / func.sum(MailChimpCampaignReport.email_sent),
+                4,
+            ).label("txOuverture"),
+            func.round(
+                func.sum(MailChimpCampaignReport.click_unique)
+                / func.sum(MailChimpCampaignReport.email_sent),
+                4,
+            ).label("txClique"),
+            func.round(
+                func.sum(MailChimpCampaignReport.unsubscribed)
+                / func.sum(MailChimpCampaignReport.email_sent),
+                4,
+            ).label("txDesabonnement"),
+        )
+        .select_from(MailChimpCampaignReport)
+        .join(MailChimpCampaignReport.mailchimp_campaign)
+        .join(MailChimpCampaign.message.and_(AdherentMessages.type == scope["code"]))
+        .first()
+    )
 
-    return {'local': res, 'national': nat}
+    return {"local": res, "national": nat}
