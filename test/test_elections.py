811f56810bf60cf0ae5b0f591ae05e5d7c9e987a
Mickaël Labarrère <35226465+mlabarrere@users.noreply.github.com>
2023-04-05 18:36:07 +0200

    Refacto en boucle For des Test Elections

--- test/test_elections.py
+++ test/test_elections.py
@@ -1,65 +1,25 @@
 from app.crud import elections
 
-def test_ElectionAgregat_1():
-    result = elections.ElectionAgregat('Municipales 2020', 'bureau')
-    assert result == 'nuance, nom, prenom'
+election_aggregat_tests = [
+    ('Municipales 2020', 'bureau', 'nuance, nom, prenom'),
+    ('Municipales 2020', 'commune', 'nuance, nom, prenom'),
+    ('Municipales 2020', 'canton', 'nuance'),
+    ('Départementales 2015', 'commune', 'nuance, num_dep_binome_candidat'),
+    ('Départementales 2015', 'canton', 'nuance, num_dep_binome_candidat'),
+    ('Départementales 2015', 'circonscription', 'nuance'),
+    ('Législatives 2017', 'canton', 'nuance, nom, prenom'),
+    ('Législatives 2017', 'circonscription', 'nuance, nom, prenom'),
+    ('Législatives 2017', 'departement', 'nuance'),
+    ('Régionales 2015', 'departement', 'nuance, nom, prenom'),
+    ('Régionales 2015', 'region', 'nuance'),
+    ('Régionales 2021', 'departement', 'nuance, nom, prenom'),
+    ('Régionales 2021', 'region', 'nuance'),
+    ('Européennes 2014', 'canton', 'nuance, nom, prenom'),
+    ('Européennes 2019', 'region', 'nom_liste'),
+    ('Présidentielles 2017', 'departement', 'nuance, nom, prenom')
+]
 
-def test_ElectionAgregat_2():
-    result = elections.ElectionAgregat('Municipales 2020', 'commune')
-    assert result == 'nuance, nom, prenom'
-
-def test_ElectionAgregat_3():
-    result = elections.ElectionAgregat('Municipales 2020', 'canton')
-    assert result == 'nuance'
-
-def test_ElectionAgregat_4():
-    result = elections.ElectionAgregat('Départementales 2015', 'commune')
-    assert result == 'nuance, num_dep_binome_candidat'
-
-def test_ElectionAgregat_5():
-    result = elections.ElectionAgregat('Départementales 2015', 'canton')
-    assert result == 'nuance, num_dep_binome_candidat'
-
-def test_ElectionAgregat_6():
-    result = elections.ElectionAgregat('Départementales 2015', 'circonscription')
-    assert result == 'nuance'
-
-def test_ElectionAgregat_7():
-    result = elections.ElectionAgregat('Législatives 2017', 'canton')
-    assert result == 'nuance, nom, prenom'
-
-def test_ElectionAgregat_8():
-    result = elections.ElectionAgregat('Législatives 2017', 'circonscription')
-    assert result == 'nuance, nom, prenom'
-
-def test_ElectionAgregat_9():
-    result = elections.ElectionAgregat('Législatives 2017', 'departement')
-    assert result == 'nuance'
-
-def test_ElectionAgregat_10():
-    result = elections.ElectionAgregat('Régionales 2015', 'departement')
-    assert result == 'nuance, nom, prenom'
-
-def test_ElectionAgregat_11():
-    result = elections.ElectionAgregat('Régionales 2015', 'region')
-    assert result == 'nuance'
-
-def test_ElectionAgregat_12():
-    result = elections.ElectionAgregat('Régionales 2021', 'departement')
-    assert result == 'nuance, nom, prenom'
-
-def test_ElectionAgregat_13():
-    result = elections.ElectionAgregat('Régionales 2021', 'region')
-    assert result == 'nuance'
-
-def test_ElectionAgregat_14():
-    result = elections.ElectionAgregat('Européennes 2014', 'canton')
-    assert result == 'nuance, nom, prenom'
-
-def test_ElectionAgregat_15():
-    result = elections.ElectionAgregat('Européennes 2019', 'region')
-    assert result == 'nom_liste'
-
-def test_ElectionAgregat_16():
-    result = elections.ElectionAgregat('Présidentielles 2017', 'departement')
-    assert result == 'nuance, nom, prenom'
+def test_ElectionAgregat():
+    for election, agregat, expected_result in election_aggregat_tests:
+        result = elections.ElectionAgregat(election, agregat)
+        assert result == expected_result
